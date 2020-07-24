0820e9e31c0d92891999d8b1ae82fe50672ac744
aquiles <jjoseaquiless@gmail.com>
2020-07-24 01:41:06 -0300

    responding to huge chamges in mangahost

--- remclass.js
+++ remclass.js
@@ -1,3 +1,64 @@
-// $(".navbar-fixed-top").removeClass("navbar-fixed-top");
-elem = document.getElementsByClassName("navbar-fixed-top");
-elem[0].classList.remove("navbar-fixed-top");
+
+class ClassWatcher {
+
+  constructor(targetNode, classToWatch, classAddedCallback, classRemovedCallback) {
+      this.targetNode = targetNode
+      this.classToWatch = classToWatch
+      this.classAddedCallback = classAddedCallback
+      this.classRemovedCallback = classRemovedCallback
+      this.observer = null
+      this.lastClassState = targetNode.classList.contains(this.classToWatch)
+
+      this.init()
+  }
+
+  init() {
+      this.observer = new MutationObserver(this.mutationCallback)
+      this.observe()
+  }
+
+  observe() {
+      this.observer.observe(this.targetNode, { attributes: true })
+  }
+
+  disconnect() {
+      this.observer.disconnect()
+  }
+
+  mutationCallback = mutationsList => {
+      for(let mutation of mutationsList) {
+          if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+              let currentClassState = mutation.target.classList.contains(this.classToWatch)
+              if(this.lastClassState !== currentClassState) {
+                  this.lastClassState = currentClassState
+                  if(currentClassState) {
+                      this.classAddedCallback()
+                  }
+                  else {
+                      this.classRemovedCallback()
+                  }
+              }
+          }
+      }
+  }
+}
+
+////////////main///////////////
+let targetNode = document.getElementById('navigation');
+
+function workOnClassAdd() {
+  targetNode.classList.remove("fixed-top");
+}
+
+function workOnClassRemoval() {
+  // do nothing. don't know what to do for now.
+  return;
+}
+
+// watch for a specific class change
+let classWatcher = new ClassWatcher(
+  targetNode,
+  'fixed-top',
+  workOnClassAdd,
+  workOnClassRemoval
+);
